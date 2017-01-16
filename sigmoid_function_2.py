c629fd8c4ee9c4b7925d91a658c7a00f40e03e8c
Kimura.Tetsuya <t.kimura@axelmark.co.jp>
2017-01-16 18:52:48 +0900

    add sigmoid関数の勉強

---
+++ sigmoid_function_2.py
@@ -0,0 +1,28 @@
+#coding: UTF-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def sigmoid(a):
+    s = 1 / (1 + e**-a)
+    return s
+
+e = math.e
+dx = 0.1
+x = np.arange(-8,8,dx)
+
+#シグモイド関数　y = 1 / (1 + e^(-x))
+#y_sig = 1 / (1+ e**-x)
+
+#シグモイド関数
+y_sig = sigmoid(x)
+
+#シグモイド関数の傾き
+y_dsig = (sigmoid(x+dx) - sigmoid(x)) / dx
+
+plt.plot(x, y_sig, label = "sigmoid")
+plt.plot(x, y_dsig, label = "d_sigmoid")
+plt.legend()
+plt.show()
+
